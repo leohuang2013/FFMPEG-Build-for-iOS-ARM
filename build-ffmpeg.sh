8079fc5593e267caa407ef2dcffe92d578dfc370
leohwang <leohwang@360converter.com>
2023-03-03 17:46:27 +0800

    Added options suitable for our project

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -19,7 +19,7 @@ THIN=`pwd`/"thin"
 #FDK_AAC=`pwd`/../fdk-aac-build-script-for-iOS/fdk-aac-ios
 
 CONFIGURE_FLAGS="--enable-cross-compile --disable-debug --disable-programs \
-                 --disable-doc --enable-pic"
+                 --disable-doc --enable-pic --enable-shared --disable-lzma --enable-gpl --disable-sdl2"
 
 if [ "$X264" ]
 then
